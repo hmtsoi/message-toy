c8292555d3e7ed42d3c04b5e850064e85c9742df
Jerry H.M. Tsoi <hungming.tsoi@gmail.com>
2019-01-28 00:02:11 +0100

    Implement message send

---
+++ src/main/java/thmlogwork/message/toy/rest/v1/messages/MessageRequest.java
@@ -0,0 +1,4 @@
+package thmlogwork.message.toy.rest.v1.messages;
+
+public class MessageRequest {
+}
--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $.ajax({
     url: "/v1/messages",
     success: function( result ) {
-        console.log(result);
       result.messages.forEach(function(m) {
         $( "#message-list-wrapper" ).append( "<h4><font color=\"#D0D0D0\">" + m.senderName + "</font></h4>" );
         $( "#message-list-wrapper" ).append( "<h4>" + m.message + "</h5>" );
@@ -10,4 +9,9 @@ $(document).ready(function() {
       })
     }
   });
+
+  $("#sendBtn").on("click", function() {
+    var input = $( "#sendInput" ).val();
+    console.log(input);
+  })
 })
