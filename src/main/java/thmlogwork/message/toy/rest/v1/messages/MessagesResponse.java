5caf970605c950468360b1e4bc1841abf4e913b9
Jerry H.M. Tsoi <hungming.tsoi@gmail.com>
2019-01-27 22:15:27 +0100

    Implement "MessagesResponse"

--- src/main/java/thmlogwork/message/toy/rest/v1/messages/MessagesResponse.java
+++ src/main/java/thmlogwork/message/toy/rest/v1/messages/MessagesResponse.java
@@ -11,6 +11,16 @@ import java.util.List;
 @JsonInclude( JsonInclude.Include.NON_NULL )
 class MessagesResponse {
 
-    private List<String> messages;
+    private List<Message> messages;
+
+    @AllArgsConstructor
+    @Getter
+    @JsonInclude( JsonInclude.Include.NON_NULL )
+    public static class Message {
+
+        private String message;
+        private String senderName;
+        private String messageTimestamp;
+    }
 
 }
--- src/main/java/thmlogwork/message/toy/rest/v1/messages/MessagesResponseFactory.java
+++ src/main/java/thmlogwork/message/toy/rest/v1/messages/MessagesResponseFactory.java
@@ -2,8 +2,11 @@ package thmlogwork.message.toy.rest.v1.messages;
 
 import thmlogwork.message.toy.rest.domain.Message;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.List;
 
+import static java.time.ZoneOffset.UTC;
 import static java.util.stream.Collectors.toList;
 
 class MessagesResponseFactory {
@@ -13,9 +16,21 @@ class MessagesResponseFactory {
 
     static MessagesResponse map( List<Message> messageList ) {
 
-        final List<String> messages = messageList.stream()
-                .map( m -> m.getMessage() )
+        final List<MessagesResponse.Message> messages = messageList.stream()
+                .map( MessagesResponseFactory::map )
                 .collect( toList() );
         return new MessagesResponse( messages );
+    }
+
+    static MessagesResponse.Message map( Message message ) {
+
+        final String timestamp = message.getMessageTimestamp()
+                .atZone( UTC )
+                .format( DateTimeFormatter.ofLocalizedDateTime( FormatStyle.MEDIUM ) );
+        return new MessagesResponse.Message(
+                message.getMessage(),
+                message.getSenderName(),
+                timestamp
+        );
     }
 }
